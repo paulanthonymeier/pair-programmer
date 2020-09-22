bcf6e601ba7e118964d9b3008a116ccbd57f98b0
Paul Meier <paulmeier@Pauls-MacBook-Pro.local>
2020-09-23 09:10:29 +1000

    refactoring randomPairsHandler function - implementing mobile responsive nav menu

--- src/students.js
+++ src/students.js
@@ -69,7 +69,7 @@ const addStudentHandler = () => {
       return;
   }
   const newStudent = {
-    id: Math.random().toString(),
+    id: Math.floor((Math.random() * 10000000000) + 1),
     firstName: firstNameValue,
     lastName: lastNameValue,
     emailAddress: emailAddressValue
@@ -82,8 +82,82 @@ const addStudentHandler = () => {
 };
 
 const randomPairsHandler = () => {
-  // pairing logic
+  console.log(students);
+  if (students.length % 2 != 0 || students.length === 0) {
+    if (students.length === 0) {
+      alert(
+        `
+        Add some students first, FOOL!
+        `
+      );
+    } else if (students.length === 1) {
+      alert(
+        `
+        You must have an even number of students!
+        You currently have only ${students.length} student.
+        `
+      );
+    } else {
+      alert(
+        `
+        You must have an even number of students!
+        You currently have ${students.length} students.
+        `
+      );
+    }
+  } 
+  else {
+    var arr1 = students.firstName.slice(0, students.length / 2), // copy array
+    arr2 = students.firstName.slice(students.length / 2, students.length); // copy array again
+    console.log('ARRAY 1', arr1);
+    console.log('ARRAY 2', arr2);
+
+    // arr1.sort(function() {
+    //   return 0.5 - Math.random();
+    // }); // shuffle arrays
+    // arr2.sort(function() {
+    //   return 0.5 - Math.random();
+    // });
+
+    // // creating an empty array to save the pairs in it
+    // const pairs = [];
+
+    // while (arr1.length) {
+    //   var name1 = arr1.pop(), // get the last value of arr1
+    //     name2 = arr2[0] == name1 ? arr2.pop() : arr2.shift();
+    //   //        ^^ if the first value is the same as name1,
+    //   //           get the last value, otherwise get the first
+
+    //   pairs.push(`${capitalize(name1)} gets ${capitalize(name2)}`); //  updating the pairs array with each pair by using push, here i am using a function capitalize to captalize each name, the function is at the bottom of this file
+    // }
+
+    // console.log(pairs);
+
+    // // creating a variable to catch the empty place in html using id-pairs
+    // const display = document.getElementById("pairs");
+
+    // //  setting a variable to create an empty element in html, mapping the whole pairs array, and setting each pair as an inner text value of each element
+    // pairs.map(pair => {
+    //   const element = document.createElement("div");
+    //   element.innerText = pair;
+    //   // <div>pair</div>
+
+    //   // at last appendchild(element) is giving us every pair in the div of html by adding the pairs in display varable
+    //   display.appendChild(element);
+    // });
+  }
+};
+
+const capitalize = (word) => {
+  return (
+    word
+      .trim()
+      .charAt(0)
+      .toUpperCase() + word.trim().slice(1)
+  );
 };
 
 btnCreateNewStudent.addEventListener('click', addStudentHandler);
+
 btnGeneratePairs.addEventListener('click', randomPairsHandler);
+
